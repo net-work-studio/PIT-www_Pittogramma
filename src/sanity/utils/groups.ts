9591acd17738451ca0cc7c4cc2f7ffb9218931fa
az3ta <24527778+az3ta@users.noreply.github.com>
2026-01-22 11:48:06 +0100

    refactor: integrate groups into interview and project schemas for better organization of fields

--- src/sanity/utils/groups.ts
+++ src/sanity/utils/groups.ts
@@ -1,10 +1,20 @@
+import { DatabaseIcon, DocumentTextIcon, SearchIcon } from "@sanity/icons";
+
 export const groups = [
+  {
+    name: "metadata",
+    title: "Metadata",
+    icon: DatabaseIcon,
+  },
   {
     name: "content",
     title: "Content",
+    default: true,
+    icon: DocumentTextIcon,
   },
   {
     name: "seo",
     title: "SEO",
+    icon: SearchIcon,
   },
 ];
