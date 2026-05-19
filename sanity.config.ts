7e1195031e81e7177251ad817922eb8d42e500cf
az3ta <24527778+az3ta@users.noreply.github.com>
2026-05-19 20:12:53 +0200

    chore(deps)!: upgrade sanity and related libs
    BREAKING CHANGE: Removed unsplash image asset source from Studio; drop
    import and plugin usage

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,6 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { presentationTool } from "sanity/presentation";
 import { structureTool } from "sanity/structure";
-import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
 import { media } from "sanity-plugin-media";
 import Mark from "@/components/brand/mark";
 import { resolve } from "@/sanity/presentation/resolve";
@@ -29,7 +28,6 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({ structure }),
-    unsplashImageAsset(),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
