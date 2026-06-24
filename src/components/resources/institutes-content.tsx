53b8502e157a97122d111174a0c52d7f1bd88125
Andrea Zaccuri <24527778+az3ta@users.noreply.github.com>
2026-06-24 07:18:06 +0000

    Rename institutes foundation label
    Co-authored-by: linear-code[bot] <222613912+linear-code[bot]@users.noreply.github.com>

--- src/components/resources/institutes-content.tsx
+++ src/components/resources/institutes-content.tsx
@@ -30,7 +30,9 @@ function InstituteListCard({ institute }: { institute: Institute }) {
       <li className="col-span-2">
         <CountryDisplay place={institute.place} />
       </li>
-      <li className="col-span-2">{institute.yearFoundation || "-"}</li>
+      <li className="col-span-2 text-right">
+        {institute.yearFoundation || "-"}
+      </li>
     </ResourceListItem>
   );
 }
@@ -108,7 +110,7 @@ export function InstitutesContent({
             <li className="col-span-2">Language</li>
             <li className="col-span-2">City</li>
             <li className="col-span-2">Country</li>
-            <li className="col-span-2">Foundation</li>
+            <li className="col-span-2 text-right">Since</li>
           </ul>
         )}
       </div>
